0057a1c6f0529f84b6eda8d32de4dd3b57c1903b
Michael Zhao <michaelzhao314@gmail.com>
2020-09-14 00:26:08 -0500

    Remove unneccesary auth call

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,40 @@ import React from 'react';
 import { login } from '../components/spotify-api';
 import { connect } from 'react-redux';
 import { getError, getLoggedIn } from '../redux/selectors';
+import { Redirect } from 'react-router';
+import { getKey } from '../components/spotify-middleware';
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: (this.props.error ? <p className="login-error">Error logging in!</p> : null)
-        }
+            error: this.props.error ? <p className="login-error">Error logging in!</p> : null,
+        };
     }
+
+    loginButton = () => {
+        if (this.props.loggedIn) {
+            this.setState({ error: <Redirect to="/user" /> });
+        } else {
+            getKey().then((refresh) => {
+                if (refresh) this.setState({ error: <Redirect to="/user" /> });
+                else login();
+            });
+        }
+    };
+
     render() {
         return (
             <div className="Login">
                 <h1>Playlists Plus!</h1>
-                <button onClick={login}>Log In to Spotify</button>
+                <button onClick={this.loginButton}>Log In to Spotify</button>
                 {this.state.error}
             </div>
         );
     }
 }
 
-export default connect((state) => ({ error: getError(state), loggedIn: getLoggedIn(state) }), null)(Login);
+export default connect(
+    (state) => ({ error: getError(state), loggedIn: getLoggedIn(state) }),
+    null
+)(Login);
